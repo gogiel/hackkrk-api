0c2dbb240a26aaf4d12d60eac691db1127fd7bb1
d4rky <d4rky@d4rky.megivps.pl>
2013-07-25 18:48:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackkrkApi::Application.config.secret_key_base = 'f430b4454be8c86c2417a4a2a94c23a97ae821e0efc631f142dd0afe50e9b55bb7af7e748a04bab3984dc3db83cc4d1976b4a11a7ce669f641bde28883fecce6'
